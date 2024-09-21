91ab51c8a73bc0720a8af99c7c303a848d8f724e
Andy Park <woong1227@gmail.com>
2024-09-21 11:46:41 -0700

    Initial Commit

---
+++ PokeDex1_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeDex1_1
+//
+//  Created by Andy Park on 9/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
